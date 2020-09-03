5bea3038329446a4824e277b8678a0e277e1565c
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2020-09-04 07:03:59 +0900

    Create 13-3_2.c

---
+++ 13-3_2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    int q, r;
+    int tmp;
+
+    scanf("%d %d", &a, &b);
+
+    if (a < b)
+    {
+        tmp = a;
+        a = b;
+        b = a;
+    }
+
+    while (1)
+    {
+        r = a % b;
+        if (r == 0)
+        {
+            break;
+        }
+        a = b;
+        b = r;
+    }
+
+    printf("%d", b);
+
+    return 0;
+}
