ee52723c5ee48a766de8fffdee77fdc2f41ccc49
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2020-11-03 14:08:54 +0900

    Update ft_print_comb.c

--- ft_print_comb.c
+++ ft_print_comb.c
@@ -6,19 +6,22 @@ void	ft_print_comb(void)
 	char num2;
 	char num3;
 
-	num1 = 48;
-	while (num1 <= 55)
+	num1 = '0';
+	while (num1 <= '7')
 	{
 		num2 = num1 + 1;
-		while (num2 <= 56)
+		while (num2 <= '8')
 		{
 			num3 = num2 + 1;
-			while (num3 <= 57)
+			while (num3 <= '9')
 			{
 				write(1, &num1, 1);
 				write(1, &num2, 1);
 				write(1, &num3, 1);
-				write(1, ", ", 2);
+				if (!(num1 == '7' && num2 == '8' && num3 == '9')) 
+				{
+					write(1, ", ", 2);
+				}
 				num3++;
 			}
 			num2++;
